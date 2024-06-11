3bbd24e6a89e748177a3b53253637df39e2b54aa
Ghimiray <121449669+ghimiraybishnu@users.noreply.github.com>
2024-06-11 21:29:38 +0545

    Commit message describing your changes

--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -1,23 +1,14 @@
 import Services from "@/components/Category/Services";
 import Hero from "@/components/Home/Hero";
 import PopularPackage from "@/components/Package/PopularPackage";
-import Testimonials from "./Testimonials/Testimonials";
-import { testimonialsData } from "./Testimonials/testimonialsData";
-import BookingBox from "./Testimonials/BookingBox";
+
 export default function Home() {
   return (
     <div>
       <Hero/>
       <PopularPackage/>
       
-      <div className="md:col-span-1">
-        <BookingBox />
-      </div>
-
-      <div className="p-6">
-      <h1 className="text-2xl font-bold mb-6"></h1>
-      <Testimonials {...testimonialsData}/>
-    </div>
+     
     <Services/>
     </div>
     
