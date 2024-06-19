f4fe44c76146b5a4ec9b65dc05f887ac32706527
Navin Lamsal <navinlamsal11@gmail.com>
2024-06-19 18:35:44 +0545

    booking on progress

---
+++ src/app/api/booking/delete/route.ts
@@ -0,0 +1,50 @@
+
+import { authenticate } from "@/lib/authenticate";
+import { errorResponse } from "@/lib/errorResponse";
+import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function DELETE(request: NextRequest) {
+
+  try {
+    const payload = await authenticate(request);
+
+    return await parseResult(request);
+  } catch (error:any) {
+    return errorResponse(error);
+  }
+}
+
+
+const parseResult =async (request:NextRequest)=>{ 
+  try {
+    const extractData = await request.json();
+
+    if(!extractData.id){
+      return NextResponse.json({
+        success: false,
+        message: "id is required in the request body.",
+      });
+    }
+
+    const deleteService = await prisma.package.delete({
+      where: {
+        id: extractData.id,
+      },
+     
+  });
+
+    if (deleteService) {
+
+      return NextResponse.json({
+        success: true,
+        message: "Page removed successfully",
+      });
+    } else {
+      return errorResponse(undefined, "failed to delete the page ! Please try again", 400)
+    }
+  } catch (e:any) {
+    console.log(e);
+    return errorResponse(e);
+  }
+}
