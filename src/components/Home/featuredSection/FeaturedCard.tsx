dabe966adf7666f7ee30c721df128198c1831289
Ram Gurung <gurungram2263@gmail.com>
2024-07-24 13:56:48 +0545

    package and feature card fixed ui

--- src/components/Home/featuredSection/FeaturedCard.tsx
+++ src/components/Home/featuredSection/FeaturedCard.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { Package } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
@@ -5,22 +6,22 @@ import React from "react";
 
 const FeaturedCard = ({data, numb}:{data:Package, numb:number}) => {
   return (
-    <div className="min-w-screen min-h-screen bg-primary flex items-center p-5 lg:p-10 overflow-hidden relative">
+    <div className="min-w-screen min-h-screen flex items-center px-5 lg:px-10 overflow-hidden relative">
       <div className="w-full max-w-6xl rounded bg-white shadow-xl p-10 lg:p-20 mx-auto text-gray-800 relative md:text-left">
         <div className="md:flex items-center -mx-10">
           <div className="w-full md:w-1/2 px-14 py-4 mb-10 md:mb-0">
             <div className="relative">
               <Image
                 src={data.image}
-                className="w-full relative z-10"
+                className="w-full relative z-10 h-[350px] object-cover object-center"
                 alt={data.title}
-                layout="responsive"
+                // layout="responsive"
                 width={500}
-                height={300} // Adjusted height to maintain the landscape aspect ratio
+                height={500} // Adjusted height to maintain the landscape aspect ratio
                 quality={100}
                 
               />
-              <div className="border-4 border-secondary absolute -top-5 -bottom-5 -left-5 -right-5 md:-top-10 md:-bottom-10 md:-left-10 md:-right-10 z-0"></div>
+              <div className="border-[3px] border-secondary absolute -top-5 -bottom-5 -left-5 -right-5 md:-top-10 md:-bottom-10 md:-left-10 md:-right-10 z-0"></div>
             </div>
           </div>
           <div className="w-full md:w-1/2 px-10">
@@ -44,16 +45,16 @@ const FeaturedCard = ({data, numb}:{data:Package, numb:number}) => {
                 <span className="font-bold text-5xl leading-none align-baseline">
                   {data.SharingOffer}
                 </span>
-                <span className="text-2xl leading-none align-baseline line-through text-secondary">
+                <span className="text-2xl leading-none align-baseline line-through text-primary">
                   {data.SharingPrice}
                 </span>
               </div>
               <div className="inline-block align-bottom">
                 <Link  href={`/packages/${data.slug}`}>
                 
-                <button className="bg-secondary opacity-75 hover:opacity-100 text-white hover:text-gray-900 rounded-full px-10 py-2 font-semibold">
-                  <i className="mdi mdi-cart -ml-2 mr-2"></i> BOOk NOW
-                </button>
+                <Button className=" hover:opacity-100 hover:text-gray-900 rounded-full px-10 py-2 font-semibold">
+                  <i className="mdi mdi-cart -ml-2 mr-2"></i> BOOK NOW
+                </Button>
                 </Link>
               </div>
             </div>
