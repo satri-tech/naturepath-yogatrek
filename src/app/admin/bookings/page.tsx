a3cb08ac7572707be7f96921e3134c046e9e66e4
Ram Gurung <gurungram2263@gmail.com>
2024-07-28 16:39:31 +0545

    feat: rg/made some ui enhancements in admin panel

--- src/app/admin/bookings/page.tsx
+++ src/app/admin/bookings/page.tsx
@@ -30,9 +30,9 @@ const BookingPage = () => {
           <TableHeader>
             <TableRow>
               <TableHead>Customer</TableHead>
-              <TableHead className="hidden sm:table-cell">Type</TableHead>
-              <TableHead className="hidden sm:table-cell">Status</TableHead>
-              <TableHead className="hidden md:table-cell">Date</TableHead>
+              <TableHead className="">Type</TableHead>
+              <TableHead className="">Status</TableHead>
+              <TableHead className="">Date</TableHead>
               <TableHead className="text-right">Amount</TableHead>
             </TableRow>
           </TableHeader>
@@ -40,15 +40,15 @@ const BookingPage = () => {
             <TableRow className="bg-accent">
               <TableCell>
                 <div className="font-medium">Liam Johnson</div>
-                <div className="hidden text-sm text-muted-foreground md:inline">
+                <div className=" text-sm text-muted-foreground md:inline">
                   liam@example.com
                 </div>
               </TableCell>
-              <TableCell className="hidden sm:table-cell">Sale</TableCell>
-              <TableCell className="hidden sm:table-cell">
+              <TableCell className="">Sale</TableCell>
+              <TableCell className="">
                 <Success>Fulfilled</Success>
               </TableCell>
-              <TableCell className="hidden md:table-cell">
+              <TableCell className="">
                 <span className=" inline-block w-[125px]">2023-06-23</span>
               </TableCell>
               <TableCell className="text-right">$250.00</TableCell>
@@ -56,101 +56,101 @@ const BookingPage = () => {
             <TableRow>
               <TableCell>
                 <div className="font-medium">Olivia Smith</div>
-                <div className="hidden text-sm text-muted-foreground md:inline">
+                <div className=" text-sm text-muted-foreground md:inline">
                   olivia@example.com
                 </div>
               </TableCell>
-              <TableCell className="hidden sm:table-cell">Refund</TableCell>
-              <TableCell className="hidden sm:table-cell">
+              <TableCell className="">Refund</TableCell>
+              <TableCell className="">
                 <Danger>Declined</Danger>
               </TableCell>
-              <TableCell className="hidden md:table-cell">2023-06-24</TableCell>
+              <TableCell className="">2023-06-24</TableCell>
               <TableCell className="text-right">$150.00</TableCell>
             </TableRow>
             <TableRow>
               <TableCell>
                 <div className="font-medium">Noah Williams</div>
-                <div className="hidden text-sm text-muted-foreground md:inline">
+                <div className=" text-sm text-muted-foreground md:inline">
                   noah@example.com
                 </div>
               </TableCell>
-              <TableCell className="hidden sm:table-cell">
+              <TableCell className="">
                 Subscription
               </TableCell>
-              <TableCell className="hidden sm:table-cell">
+              <TableCell className="">
                 <Success>Fulfilled</Success>
               </TableCell>
-              <TableCell className="hidden md:table-cell">2023-06-25</TableCell>
+              <TableCell className="">2023-06-25</TableCell>
               <TableCell className="text-right">$350.00</TableCell>
             </TableRow>
             <TableRow>
               <TableCell>
                 <div className="font-medium">Emma Brown</div>
-                <div className="hidden text-sm text-muted-foreground md:inline">
+                <div className=" text-sm text-muted-foreground md:inline">
                   emma@example.com
                 </div>
               </TableCell>
-              <TableCell className="hidden sm:table-cell">Sale</TableCell>
-              <TableCell className="hidden sm:table-cell">
+              <TableCell className="">Sale</TableCell>
+              <TableCell className="">
                 <Success>Fulfilled</Success>
               </TableCell>
-              <TableCell className="hidden md:table-cell">2023-06-26</TableCell>
+              <TableCell className="">2023-06-26</TableCell>
               <TableCell className="text-right">$450.00</TableCell>
             </TableRow>
             <TableRow>
               <TableCell>
                 <div className="font-medium">Liam Johnson</div>
-                <div className="hidden text-sm text-muted-foreground md:inline">
+                <div className=" text-sm text-muted-foreground md:inline">
                   liam@example.com
                 </div>
               </TableCell>
-              <TableCell className="hidden sm:table-cell">Sale</TableCell>
-              <TableCell className="hidden sm:table-cell">
+              <TableCell className="">Sale</TableCell>
+              <TableCell className="">
                 <Success>Fulfilled</Success>
               </TableCell>
-              <TableCell className="hidden md:table-cell">2023-06-23</TableCell>
+              <TableCell className="">2023-06-23</TableCell>
               <TableCell className="text-right">$250.00</TableCell>
             </TableRow>
             <TableRow>
               <TableCell>
                 <div className="font-medium">Liam Johnson</div>
-                <div className="hidden text-sm text-muted-foreground md:inline">
+                <div className=" text-sm text-muted-foreground md:inline">
                   liam@example.com
                 </div>
               </TableCell>
-              <TableCell className="hidden sm:table-cell">Sale</TableCell>
-              <TableCell className="hidden sm:table-cell">
+              <TableCell className="">Sale</TableCell>
+              <TableCell className="">
                 <Success>Fulfilled</Success>
               </TableCell>
-              <TableCell className="hidden md:table-cell">2023-06-23</TableCell>
+              <TableCell className="">2023-06-23</TableCell>
               <TableCell className="text-right">$250.00</TableCell>
             </TableRow>
             <TableRow>
               <TableCell>
                 <div className="font-medium">Olivia Smith</div>
-                <div className="hidden text-sm text-muted-foreground md:inline">
+                <div className=" text-sm text-muted-foreground md:inline">
                   olivia@example.com
                 </div>
               </TableCell>
-              <TableCell className="hidden sm:table-cell">Refund</TableCell>
-              <TableCell className="hidden sm:table-cell">
+              <TableCell className="">Refund</TableCell>
+              <TableCell className="">
                 <Danger>Declined</Danger>
               </TableCell>
-              <TableCell className="hidden md:table-cell">2023-06-24</TableCell>
+              <TableCell className="">2023-06-24</TableCell>
               <TableCell className="text-right">$150.00</TableCell>
             </TableRow>
             <TableRow>
               <TableCell>
                 <div className="font-medium">Emma Brown</div>
-                <div className="hidden text-sm text-muted-foreground md:inline">
+                <div className=" text-sm text-muted-foreground md:inline">
                   emma@example.com
                 </div>
               </TableCell>
-              <TableCell className="hidden sm:table-cell">Sale</TableCell>
-              <TableCell className="hidden sm:table-cell">
+              <TableCell className="">Sale</TableCell>
+              <TableCell className="">
                 <Success>Fulfilled</Success>
               </TableCell>
-              <TableCell className="hidden md:table-cell">2023-06-26</TableCell>
+              <TableCell className="">2023-06-26</TableCell>
               <TableCell className="text-right">$450.00</TableCell>
             </TableRow>
           </TableBody>
