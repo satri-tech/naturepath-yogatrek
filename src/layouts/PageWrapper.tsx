20e0bb1885ac06d404d487bf0d379b99e37cc48b
Ram Gurung <gurungram2263@gmail.com>
2024-07-28 16:39:31 +0545

    feat: rg/made some ui enhancements in admin panel

--- src/layouts/PageWrapper.tsx
+++ src/layouts/PageWrapper.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PageWrapper = ({children}:{children:React.ReactNode}) => {
   return (
-    <div className='container my-3  py-4 px-0.5 sm:px-1 md:px-4'>
+    <div className='container p-4 md:p-5 bg-primary/10'>
       {children}
     </div>
   )
--- src/layouts/admin/User.tsx
+++ src/layouts/admin/User.tsx
@@ -1,9 +1,17 @@
 "use client";
-import { LogOut } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Home, LogOut } from "lucide-react";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { LayoutDashboard } from "lucide-react";
 
 const User = () => {
   const { data: session } = useSession();
@@ -11,18 +19,54 @@ const User = () => {
     <>
       {session && session.user ? (
         <div className="flex gap-4 flex-1 items-center justify-end">
-          {session?.user?.firstName}
-          <Image
-            src="https://randomuser.me/api/portraits/men/20.jpg"
-            alt=""
-            width={100}
-            height={100}
-            quality={100}
-            className="rounded-full w-12 h-12 object-cover "
-          />
-          <Link href={"/api/auth/signout"}>
-            <LogOut />
-          </Link>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Image
+                src="https://randomuser.me/api/portraits/men/20.jpg"
+                alt=""
+                width={100}
+                height={100}
+                quality={100}
+                className="rounded-full w-10 h-10 object-cover "
+              />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem className=" flex items-start gap-2 cursor-default">
+                <Image
+                  src="https://randomuser.me/api/portraits/men/20.jpg"
+                  alt=""
+                  width={100}
+                  height={100}
+                  quality={100}
+                  className="rounded-full w-10 h-10 object-cover "
+                />
+                <span className=" font-semibold text-black/90">
+                  {session?.user?.firstName} {session?.user?.lastName}
+                </span>
+              </DropdownMenuItem>
+              <DropdownMenuItem className=" flex items-center gap-2">
+                <Link href={"/"} className=" flex items-center gap-2">
+                  <Home className=" w-5 h-5" />
+                  <span>Home</span>
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Link href={"/admin"} className=" flex items-center gap-2">
+                  <LayoutDashboard className=" w-5 h-5" />
+                  <span>Dashboard</span>
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem className="  bg-yoga-red/10 text-yoga-red hover:!text-yoga-red hover:!bg-yoga-red/20">
+                <Link
+                  href={"/api/auth/signout"}
+                  className="flex items-center gap-2"
+                >
+                  <LogOut className=" w-5 h-5" />
+                  <span>Logout</span>
+                </Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       ) : (
         <div>hello</div>
